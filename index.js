bdcbb165b53f3cd9df5a7f831d1cb293360744fc
Abdelrahman Tarek <abdelrahman.tarek1910@gmail.com>
2020-02-14 03:45:50 +0200

    get all genres api

---
+++ index.js
@@ -0,0 +1,19 @@
+const Joi = require('joi');
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+const genres = [
+    { id: 1, name: 'Action' },  
+    { id: 2, name: 'Horror' },  
+    { id: 3, name: 'Romance' },  
+];
+
+app.get('/api/genres', (req, res) => {
+    res.send(genres);
+});
+
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`listening on port ${port}...`));
