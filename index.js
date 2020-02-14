dfdf09e83143638264c00d57aed59f31232d2ba2
Abdelrahman Tarek <abdelrahman.tarek1910@gmail.com>
2020-02-14 03:59:55 +0200

    genres post request API

--- index.js
+++ index.js
@@ -14,6 +14,34 @@ app.get('/api/genres', (req, res) => {
     res.send(genres);
 });
 
+app.get('/api/genres/:id', (req, res) => {
+    const genre = genres.find(g => g.id === parseInt(req.params.id));
+    if (!genre) return res.status(404).send('The Genre with the given ID is not found');
+
+    res.send(genre);
+});
+
+app.post('/api/genres', (req, res) => {
+    const { error } = validateGenre(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+
+    const genre = {
+        id: genres.length + 1,
+        name: req.body.name
+    };
+    genres.push(genre);
+    res.send(genre);
+});
+
+
+
+function validateGenre(genre)
+{
+    const schema = {
+        name: Joi.string().min(3).max(20).required()
+    }
+    return Joi.validate(genre, schema);
+}
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`listening on port ${port}...`));
