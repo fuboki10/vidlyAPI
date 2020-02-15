acc0e4c35915c80dfc5e65a7ec07fcb065ddbb4e
Abdelrahman Tarek <abdelrahman.tarek1910@gmail.com>
2020-02-15 23:33:13 +0200

    Add Customers API

--- index.js
+++ index.js
@@ -7,11 +7,16 @@ mongoose.connect('mongodb://localhost/vidly')
     .then((result) => console.log('Connected to db...'))
     .catch((err) => console.error('Error : ', err.message));
 
+// Get the Routes
 const genres = require('./routes/genres');
+const customers = require('./routes/customers');
 
+// App Middleware Functions
 app.use(express.json());
 app.use('/api/genres', genres);
+app.use('/api/customers', customers);
 
+// Listen to port
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
     return console.log(`listening on port ${port}...`);
--- routes/genres.js
+++ routes/genres.js
@@ -62,7 +62,7 @@ function validateGenre(genre)
 {
     const schema = {
         name: Joi.string().min(3).max(20).required()
-    }
+    };
     return Joi.validate(genre, schema);
 };
 
