64df85c0f9b7fff2e73dad2b775680013bf0e11b
Abdelrahman Tarek <abdelrahman.tarek1910@gmail.com>
2020-02-16 23:52:48 +0200

    add objectId validation

--- models/movie.js
+++ models/movie.js
@@ -1,4 +1,5 @@
 const Joi = require('joi');
+Joi.objectId = require('joi-objectid')(Joi);
 const mongoose = require('mongoose');
 const { genreSchema } = require('./genre');
 
@@ -36,7 +37,7 @@ const Movie = mongoose.model('Movie', movieSchema);
 function validate(movie) {
   const schema = {
     title: Joi.string().min(1).max(50).required(),
-    genreId: Joi.string().required(),
+    genreId: Joi.objectId().required(),
     numberInStock: Joi.number().min(0).max(255).required(),
     dailyRentalRate: Joi.number().min(0).max(255).required()
   };
