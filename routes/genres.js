903af80f87e87d22f8306596f064239eac0a4960
Abdelrahman Tarek <abdelrahman.tarek1910@gmail.com>
2020-02-21 00:30:20 +0200

    Fix Genre GET /:id Changed id to req.params.id

--- routes/genres.js
+++ routes/genres.js
@@ -4,13 +4,26 @@ const { Genre, validate } = require('../models/genre');
 const auth = require('../middleware/auth');
 const admin = require('../middleware/admin');
 
+/**
+ * @public
+ * @returns array of Genres
+ * @param {req} Request
+ * @param {res} Response
+ */
 router.get('/', async (req, res) => {
     const genres = await Genre.find();
     res.send(genres);
 });
 
+/**
+ * @public
+ * @returns genre with the given id
+ * @param {req} Request
+ * @param {res} Response
+ * @param {id} genreId
+ */
 router.get('/:id', async (req, res) => {
-    const genre = await Genre.findById(id);
+    const genre = await Genre.findById(req.params.id);
     if (!genre) return res.status(404).send('The Genre with the given ID is not found');
     res.send(genre);
 });
